22f5f62ab9b008860c1645a429bf00d3a8305146
pedrofavari <pedro.favari@netshoes.com>
2019-03-09 10:40:12 -0300

    kick off

---
+++ src/main/java/com/netshoes/nerdtalkspringboot/NerdtalkSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.netshoes.nerdtalkspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NerdtalkSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NerdtalkSpringbootApplication.class, args);
+	}
+
+}
